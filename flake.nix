5c788cc5bee6b59e0fd7f57fd4328cb1a5d51936
Steven Crake <ekarcnevets@gmail.com>
2026-09-16 20:13:58 +0100

    ci: publish via OIDC and standardise the toolchain on Node 24 (#90)

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs_22
+            nodejs_24
             pnpm_10
             nodePackages.typescript
             nodePackages.typescript-language-server
